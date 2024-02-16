a61c9984906092655416af9724d556c804b6be34
xoo2001 <ekora2001@gmail.com>
2024-02-16 22:59:57 +0700

    X695C: add missing permissions required to boot

--- device.mk
+++ device.mk
@@ -75,6 +75,12 @@ PRODUCT_COPY_FILES += \
 # Vendor overlay
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/vendor-overlay/,$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION))
+    
+# [DNM] Temp permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/permissions/xyz.extras.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/xyz.extras.xml \
+    $(LOCAL_PATH)/permissions/xyz.extras.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/xyz.extras.xml \
+    $(LOCAL_PATH)/permissions/xyz.extras.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/xyz.extras.xml
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/infinix/X695C/X695C-vendor.mk)

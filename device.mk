5022d895db9559c3d67367143677f41df5f78657
xoo2001 <ekora2001@gmail.com>
2024-02-16 22:45:55 +0700

    X695C: Add Mediatek IMS permissions

--- device.mk
+++ device.mk
@@ -56,6 +56,9 @@ PRODUCT_BOOT_JARS += \
     mediatek-telephony-base \
     mediatek-telephony-common
     
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml
+    
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd

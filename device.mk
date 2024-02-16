1378d8821c3903f63377a589e7115dc80007fded
xoo2001 <ekora2001@gmail.com>
2024-02-16 20:07:39 +0700

    X695C: configs: import keylayout from stock

--- device.mk
+++ device.mk
@@ -11,6 +11,11 @@ AB_OTA_POSTINSTALL_CONFIG += \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
+    
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
+    $(DEVICE_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/infinix/X695C/X695C-vendor.mk)
